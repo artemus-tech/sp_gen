9f45f9da34a15de1f09f76591c748a8885fb16de
artemus-tech <artemkav@gmail.com>
2025-03-22 13:36:35 +0300

    connect_timeout=0  #

--- Autoaxillary/saxs.py
+++ Autoaxillary/saxs.py
@@ -11,3 +11,10 @@ def init_q_vector(settings)->np.ndarray:
     q_max = settings["qn"]
     q_number = settings["q_num"]
     return np.logspace(np.log10(q_min), np.log10(q_max),q_number)
+
+
+def sp_volume(r):
+    return 4.0/3.0*np.pi*np.power(r,3)
+
+def sp_factor(qr):
+    return 9.0*(np.sin(qr)-qr*np.cos(qr)/np.power(qr,3),2)
